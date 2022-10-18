ad4aff73cafff4b1125d6740843f39c45a9504c4
Tiago Lino <tiagobrasil18@gmail.com>
2022-10-18 07:27:22 -0300

    final version of study session

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Components Dinâmicos</h1>
+
+    <button @click="componentSelecionado = 'AppHome'">Home</button>
+    <button @click="componentSelecionado = 'AppPostsLista'">PostsLista</button>
+    <button @click="componentSelecionado = 'AppSobre'">Sobre</button>
+    <button @click="componentSelecionado = 'AppAssincrono'">Assincrono</button>
+    <button @click="componentSelecionado = 'AppContato'">Contato</button>
+
+    <KeepAlive max="2">
+      <component
+        :is="componentSelecionado"
+        v-bind="propsAtuais"
+        ></component>
+    </KeepAlive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHome from "./components/AppHome.vue";
+import AppPostsLista from "./components/AppPostsLista.vue";
+import AppSobre from "./components/AppSobre.vue";
+
+const AppContato = {
+  render: h => h({
+    name: 'ContatoDados',
+    template: '<h2>Component Anonimo</h2>'
+  })
+}
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppAssincrono: () => ({
+      // eslint-disable-next-line no-unused-vars
+      component: new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(import('./components/AppAssincrono.vue'))
+          // reject('Carregamento falhou!')
+        }, 2000)
+      }),
+      loading: { template: '<p>Carregando...</p>' },
+      error: { template: '<p>Erro ao carregar component!</p>' },
+      delay: 200,
+      timeout: 3000
+    }),
+    AppContato,
+    AppHome,
+    AppPostsLista,
+    AppSobre,
+  },
+  data() {
+    return {
+      componentSelecionado: "AppHome",
+      posts: [
+        {
+          id: 1,
+          titulo: "Components no Vue",
+          conteudo: "Components são uma das peças mais importantes",
+          autor: "Tiago Neves",
+        },
+        {
+          id: 2,
+          titulo: "Distribuindo conteúdo com Slots",
+          conteudo: "Slots podem ser acessados como repositórios de código HTML",
+          autor: "Tiago Neves",
+        },
+      ],
+    };
+  },
+  computed: {
+    propsAtuais() {
+      return this.componentSelecionado === "AppPostsLista" ? { posts: this.posts } : {};
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  width: 960px;
+  margin: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// eslint-disable-next-line no-unused-vars
+// Vue.component('AppAssincrono', (resolve, reject) => {
+//   setTimeout(() => {
+//     resolve({
+//       template: '<h2>Component Assincrono</h2>'
+//     })
+//   }, 2000)
+// })
+
+// Vue.component(
+//   'AppAssincrono',
+//   () => import('./components/AppAssincrono.vue')
+// )
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
